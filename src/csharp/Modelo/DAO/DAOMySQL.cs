56e4a4cce1b9cb9b6df5ebc578ab1bb813f3595b
luchoxx87 <neoluis87@gmail.com>
2018-10-22 14:12:20 -0300

    Update src/csharp/Modelo/DAO/DAOMySQL.cs

--- src/csharp/Modelo/DAO/DAOMySQL.cs
+++ src/csharp/Modelo/DAO/DAOMySQL.cs
@@ -7,9 +7,12 @@ namespace DAO
 {
     public class DAOMySQL: IDAO
     {
+        MySqlConnection conexion;
+        MySqlCommand comando;
+        MySqlDataAdapter adaptador;
         public void guardarMediciones(List<Medicion> mediciones)
         {
-            throw new NotImplementedException();
+            mediciones.ForEach(m => this.guardarMediciones(m));
         }
 
         public void guardarTipoMedicion(TipoMedicion tipoMedicion)
@@ -17,6 +20,11 @@ namespace DAO
             throw new NotImplementedException();
         }
         public List<TipoMedicion> traerTipoMediciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void guardarMediciones(Medicion medicion)
         {
             throw new NotImplementedException();
         }
