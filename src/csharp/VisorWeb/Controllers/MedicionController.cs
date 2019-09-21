06e5c05957a6602c51d915febb5c2f355dc63cb3
jvelazquez <jonathanvgms@gmail.com>
2019-09-21 02:48:48 -0300

    Nueva interfaz web

---
+++ src/csharp/VisorWeb/Controllers/MedicionController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using VisorWeb.Models;
+
+namespace WebTemperatura.Controllers
+{
+    public class MedicionController : Controller
+    {
+        private readonly MedicionesContext context;
+
+        public MedicionController(MedicionesContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: Medicion
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Grafico()
+        {
+            return View(); 
+        }
+
+        public IActionResult Tabla()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        //public JsonResult GetUltimaMedicion()
+        //{
+        //    DateTime fechaReferencia = DateTime.Now.AddSeconds(-10);
+        //    Medicion med = context.Medicion
+        //        .FirstOrDefault(x => x.FechaHora > fechaReferencia);
+        //    return Json( new { Response = new { idMedicion = med.IdMedicion, tipoMedicion = med.IdTipoMedicionNavigation.TipoMedicion1, fechaHora = med.FechaHora } } );
+        //}
+
+        [HttpGet]
+        public JsonResult GetUltimaMedicionTemperatura()
+        {
+            DateTime fechaReferencia = DateTime.Now.AddSeconds(-3);
+            Medicion med = context.Medicion.FirstOrDefault(x => x.FechaHora > fechaReferencia && x.IdTipoMedicionNavigation.TipoMedicion1 == "temperatura");
+            return Json( new { idMedicion = med.IdMedicion, valor = med.Valor, fechaHora = med.FechaHora, tipoMedicion = "temperatura" } );
+        }
+
+        [HttpGet]
+        public JsonResult GetUltimaMedicionSensacionTermica()
+        {
+
+            DateTime fechaReferencia = DateTime.Now.AddSeconds(-3);
+            Medicion med = context.Medicion.FirstOrDefault(x => x.FechaHora > fechaReferencia && x.IdTipoMedicionNavigation.TipoMedicion1 == "sensacionTermica");
+            return Json( new { idMedicion = med.IdMedicion, valor = med.Valor, fechaHora = med.FechaHora, tipoMedicion = "sensacionTermica" });
+        }
+
+        [HttpGet]
+        public JsonResult GetUltimaMedicionHumedad()
+        {
+            DateTime fechaReferencia = DateTime.Now.AddSeconds(-3);
+            Medicion med = context.Medicion.FirstOrDefault(x => x.FechaHora > fechaReferencia && x.IdTipoMedicionNavigation.TipoMedicion1 == "humedad");
+            return Json( new { idMedicion = med.IdMedicion, valor = med.Valor, fechaHora = med.FechaHora, tipoMedicion = "humedad" });
+        }
+    }
+}
---
+++ src/csharp/VisorWeb/Models/Tipomedicion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisorWeb.Models
+{
+    public partial class Tipomedicion
+    {
+        public Tipomedicion()
+        {
+            Informe = new HashSet<Informe>();
+            Medicion = new HashSet<Medicion>();
+        }
+
+        public byte IdTipoMedicion { get; set; }
+        public string TipoMedicion1 { get; set; }
+
+        public virtual ICollection<Informe> Informe { get; set; }
+        public virtual ICollection<Medicion> Medicion { get; set; }
+    }
+}
